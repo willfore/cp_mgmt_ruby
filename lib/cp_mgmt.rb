172d7c86d5ef3315ceb2c5ccb89045e7fe7a8a3b
Billy Forester <william.forester@gmail.com>
2018-11-16 10:20:29 -0600

    remove junit format

--- lib/cp_mgmt.rb
+++ lib/cp_mgmt.rb
@@ -4,8 +4,10 @@ module CpMgmt
   class Error < StandardError; end
 
   # setup for all API queries  
-ENV.store('mgmt_server_url', "https://192.168.0.8:443")
-ENV.store('mgmt_user', "admin")
-ENV.store('mgmt_pass', "vpn123")
+  ENV.store('mgmt_server_url', "https://192.168.0.8:443")
+  ENV.store('mgmt_user', "admin")
+  ENV.store('mgmt_pass', "vpn123")
+
+  
 
 end
